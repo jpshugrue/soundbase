15d398d914bc99f31da95985e7237b7c9cc21057
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-02 14:22:05 -0500

    Complete SessionAPIUtil

--- frontend/soundbase.jsx
+++ frontend/soundbase.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//begin testing
+import * as SessionApiUtil from './util/session_api_util';
+window.signup = SessionApiUtil.signup;
+window.login = SessionApiUtil.login;
+window.logout = SessionApiUtil.logout;
+//end testing
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Soundbase</h1>, root);
