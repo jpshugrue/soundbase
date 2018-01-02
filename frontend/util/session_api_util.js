47dd0149d6a4b7bbc33e424e277ed4714a700dea
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-02 14:22:05 -0500

    Complete SessionAPIUtil

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,22 @@
+export const signup = (artist) => {
+  return $.ajax({
+    url: '/api/artists',
+    method: 'POST',
+    data: artist
+  });
+};
+
+export const login = (artist) => {
+  return $.ajax({
+    url: '/api/session',
+    method: 'POST',
+    data: artist
+  });
+};
+
+export const logout = () => {
+  return $.ajax({
+    url: '/api/session',
+    method: 'DELETE'
+  });
+};
