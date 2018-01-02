cb03ad57cc00eaa56d80237950412d0c6eea4f50
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-02 15:29:42 -0500

    Add store and tested default state

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 import sessionReducer from './session_reducer';
-import errorReducer from './errors/reducer';
+import errorReducer from './errors_reducer';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
--- frontend/soundbase.jsx
+++ frontend/soundbase.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 //begin testing
-import * as SessionApiUtil from './util/session_api_util';
-window.signup = SessionApiUtil.signup;
-window.login = SessionApiUtil.login;
-window.logout = SessionApiUtil.logout;
+// import * as SessionApiUtil from './util/session_api_util';
+// window.signup = SessionApiUtil.signup;
+// window.login = SessionApiUtil.login;
+// window.logout = SessionApiUtil.logout;
 //end testing
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  //begin testing
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  //end testing
   ReactDOM.render(<h1>Soundbase</h1>, root);
 });
