c144e95f44aefe3eefeee983a08c16e23e01c424
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-02 21:03:22 -0500

    Add footer and CSS

---
+++ frontend/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <nav className="footerNav">
+    <a href="https://github.com/jpshugrue">Github</a>
+    <a href="https://www.linkedin.com/in/jeremiahshugrue/">LinkedIn</a>
+    <a href="https://github.com/jpshugrue">Portfolio</a>
+  </nav>
+);
+
+export default Footer;
