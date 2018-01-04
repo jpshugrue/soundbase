29fbc33cbc8347bbd7ca66f99826b8d231a8a281
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-04 13:20:57 -0500

    Change login behavior to route to artist show page on login/signup

--- frontend/components/header/main_header.jsx
+++ frontend/components/header/main_header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import HeaderNavContainer from './header_nav/header_nav_container';
+import { Link } from 'react-router-dom';
 
 const MainHeader = () => (
   <div className="mainHeaderBox">
     <div className="mainHeaderCenterBox">
       <div className="mainHeaderLeft">
-        <img src={window.staticImages.headerLogoHome}></img>
+        <Link to="/"><img src={window.staticImages.headerLogoHome}></img></Link>
         <span>This will be the search bar</span>
       </div>
       <div className="mainHeaderRight">
