2c102b927447832eb5439b96277ece3884f8615b
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-05 16:24:46 -0500

    Add album list components, before testing

---
+++ frontend/components/albums/album_list_container.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import { fetchAlbums } from '../../actions/album_actions';
+import { selectAlbums } from '../../reducers/selectors';
+import AlbumList from './album_list';
+
+const mapStateToProps = (state, { match }) => {
+  let albums;
+  if (match.params.artistId) {
+    albums = selectAlbums(state.entities, match.params.artistId);
+  } else {
+    albums = state.entities.albums;
+  }
+  return {
+    albums
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchAlbums: () => dispatch(fetchAlbums())
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(AlbumList);
