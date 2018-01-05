f3abe4691c8f1c637a7d9e270be850b4547cd2c5
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-05 14:39:56 -0500

    Add album seeds, controller methods, image attachment

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,4 +5,7 @@ class Album < ApplicationRecord
   belongs_to :artist
   has_many :songs
 
+  has_attached_file :album_cover, default_url: "default_album_cover.png"
+  validates_attachment_content_type :album_cover, content_type: /\Aimage\/.*\Z/
+
 end
