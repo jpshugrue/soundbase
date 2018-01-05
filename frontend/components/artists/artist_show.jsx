a93c20a208d801c5fd6be7317c828f0dd0773871
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-05 13:34:07 -0500

    Implemented artist profile box on artist show page

--- frontend/components/artists/artist_show.jsx
+++ frontend/components/artists/artist_show.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-const ArtistShow = ({ artist, artistId, fetchArtist }) => (
-  <div className="artistShowContainer">
-    <div className="artistProfilePic">
-      //Profile pic goes here
-    </div>
-    <div className="artistName">
-      //Artist name goes here
-    </div>
-  </div>
-);
+class ArtistShow extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = this.props.artist;
+  }
+
+  componentDidMount() {
+    this.props.fetchArtist(this.props.artistId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(nextProps.artist);
+  }
+
+  render() {
+    return (
+      <div className="artistShowContainer">
+        <div className="artistProfilePic">
+          <img src={this.state.profile_pic} className="profilePic"></img>
+        </div>
+        <div className="artistShowName">
+          {this.state.display_name}
+        </div>
+      </div>
+    );
+  }
+
+}
 
 export default ArtistShow;
