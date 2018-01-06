e9794f9b3262d067c6713bd2c40054ee4b6c9eb8
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-06 16:36:20 -0500

    Implement artist show page color selection

--- frontend/components/artists/artist_show_page.jsx
+++ frontend/components/artists/artist_show_page.jsx
@@ -8,25 +8,38 @@ class ArtistShowPage extends React.Component {
 
   constructor(props) {
     super(props);
+    this.backgroundStyle = {};
+    this.bodyStyle = {};
+    this.textStyle = {};
+    this.linkStyle = {};
   }
 
   componentDidMount() {
     this.props.fetchArtist(this.props.artistId);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps !== this.props) {
+      this.backgroundStyle = {backgroundColor: nextProps.artist.background_color};
+      this.bodyStyle = {backgroundColor: nextProps.artist.body_color};
+      this.textStyle = {color: nextProps.artist.text_color};
+      this.linkStyle = {color: nextProps.artist.link_color};
+    }
+  }
+
   render() {
     return (
-      <div className="artistShowPageContainer">
-        <div className="artistShowPageBodyBox">
+      <div className="artistShowPageContainer" style={this.backgroundStyle}>
+        <div className="artistShowPageBodyBox" style={this.bodyStyle}>
           <div className="artistCoverImageBox">
             <ArtistCover artist={ this.props.artist } />
           </div>
           <div className="showPageContentBox">
             <div className="albumListBox">
-              <AlbumListContainer artist={ this.props.artist } />
+              <AlbumListContainer artist={ this.props.artist } linkStyle={ this.linkStyle } />
             </div>
             <div className="artistShowBox">
-              <ArtistShow artist={ this.props.artist } />
+              <ArtistShow artist={ this.props.artist } textStyle={ this.textStyle } />
             </div>
           </div>
         </div>
