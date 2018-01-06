df8d3271a3e7dadd3a38b6d89052d16eb60f6181
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-06 15:03:48 -0500

    Add logo and corrected seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,9 @@ demoAccount = Artist.create(username: "demonstrator",
   background_color: "#1ECAC8",
   body_color: "#DAF7A6",
   text_color: "#000000",
-  link_color: "#D70EF4"
+  link_color: "#D70EF4",
+  profile_pic: File.new("#{Rails.root}/app/assets/images/square_image_seeds/File1.jpg"),
+  cover_image: File.new("#{Rails.root}/app/assets/images/cover_image_seeds/File205.jpg")
 )
 
 Album.destroy_all
@@ -22,19 +24,38 @@ demoAlbum1 = Album.create(
   album_title: "Self Titled Debut Album",
   release_date: "1999-01-08",
   album_credits: "All songs written and perfomed by The Demonstrators",
-  artist_id: demoAccount.id
+  artist_id: demoAccount.id,
+  album_cover: File.new("#{Rails.root}/app/assets/images/square_image_seeds/File100.jpg")
 )
 
 demoAlbum2 = Album.create(
   album_title: "Meet The Demonstrators",
   release_date: "2002-03-20",
   album_credits: "Produced by Steve Albini",
-  artist_id: demoAccount.id
+  artist_id: demoAccount.id,
+  album_cover: File.new("#{Rails.root}/app/assets/images/square_image_seeds/File88.jpg")
 )
 
 demoAlbum3 = Album.create(
   album_title: "The Demonstrators Greatest Hits Vol. 1",
   release_date: "2004-11-05",
   album_credits: "All tracks remixed from tape masters. Special thanks to Ampex",
-  artist_id: demoAccount.id
+  artist_id: demoAccount.id,
+  album_cover: File.new("#{Rails.root}/app/assets/images/square_image_seeds/File87.jpg")
+)
+
+demoAlbum4 = Album.create(
+  album_title: "Untitled",
+  release_date: "2014-08-01",
+  album_credits: "Recorded at The Record Plant NYC",
+  artist_id: demoAccount.id,
+  album_cover: File.new("#{Rails.root}/app/assets/images/square_image_seeds/File96.jpg")
+)
+
+demoAlbum5 = Album.create(
+  album_title: "Hits To Hum To",
+  release_date: "2016-02-10",
+  album_credits: "Guitar and Vocals: Jimmy Demonstrator, Bass: Terry Demonstrator, Drums: Roland TR-808",
+  artist_id: demoAccount.id,
+  album_cover: File.new("#{Rails.root}/app/assets/images/square_image_seeds/File5.jpg")
 )
