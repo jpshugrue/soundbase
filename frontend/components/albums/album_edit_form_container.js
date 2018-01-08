d778933f730451f621a9c059914409e1bc77cc5d
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-08 18:04:01 -0500

    Fix album edit song upload, now working on having the edit page display the existing songs

---
+++ frontend/components/albums/album_edit_form_container.js
@@ -0,0 +1,29 @@
+import { connect } from 'react-redux';
+import { updateAlbum, fetchAlbum } from '../../actions/album_actions';
+import { createSong, fetchSongs, updateSong, deleteSong } from '../../actions/song_actions';
+import { selectAlbum, selectSongs } from '../../reducers/selectors';
+import AlbumEditForm from './album_edit_form';
+
+const mapStateToProps = (state, { match }) => {
+  const albumId = parseInt(match.params.albumId);
+  const album = selectAlbum(state.entities, match.params.albumId);
+  const songs = selectSongs(state.entities, match.params.albumId);
+  return {
+    albumId,
+    album,
+    songs
+  };
+};
+
+const mapDispatchToProps = (dispatch, { match }) => {
+  return {
+    fetchAlbum: id => dispatch(fetchAlbum(id)),
+    updateAlbum: album => dispatch(updateAlbum(album)),
+    fetchSongs: albumId => dispatch(fetchSongs(albumId)),
+    updateSong: song => dispatch(updateSong(song)),
+    createSong: song => dispatch(createSong(song)),
+    deleteSong: id => dispatch(deleteSong(id))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AlbumEditForm);
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import artistReducer from './artists_reducer';
 import albumReducer from './albums_reducer';
+import songReducer from './songs_reducer';
 
 const entitiesReducer = combineReducers({
   artists: artistReducer,
-  albums: albumReducer
+  albums: albumReducer,
+  songs: songReducer
 });
 
 export default entitiesReducer;
