b6559fa3b8ee23973c57b74db69b7f1d9d0848f5
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-07 20:15:06 -0500

    Updated json views to actually flatten state correctly

---
+++ frontend/components/albums/album_form.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+class AlbumForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    
+  }
+
+}
+
+export default AlbumForm;
