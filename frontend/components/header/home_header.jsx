05f1ed36aed87b994ed50f02b3675169b2ea7b75
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-09 17:50:21 -0500

    Basic search bar functionality complete - needs CSS and deploy to other headers

--- frontend/components/header/home_header.jsx
+++ frontend/components/header/home_header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import GreetingContainer from '../greeting/greeting_container';
+import SearchBarContainer from './search_bar_container';
 
 const HomeHeader = () => (
   <div className="homeHeaderBox">
     <nav className="headerTopRow">
       <img src={window.staticImages.headerLogoHome}></img>
-      <span>This will be the search bar</span>
+      <SearchBarContainer />
     </nav>
     <GreetingContainer />
   </div>
