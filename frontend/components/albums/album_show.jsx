e41f9b899d4b36ad606b99c76a1528f0519e9808
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-11 17:23:07 -0500

    Make album show song names clickable

--- frontend/components/albums/album_show.jsx
+++ frontend/components/albums/album_show.jsx
@@ -189,7 +189,7 @@ class AlbumShow extends React.Component {
                     {this.addToSymbolArray()}
                     <button key="play" type="button" onClick={() => this.handleCurrentSong(song, idx)}>{this.smallPlaySymbol[idx]}</button>
                     <span className="trackNumber" style={this.props.textStyle}>{song.track_number}.</span>
-                    <span className="trackName" style={this.props.linkStyle}>{song.song_title}</span>
+                    <span className="trackName" onClick={() => this.handleCurrentSong(song, idx)} style={this.props.linkStyle}>{song.song_title}</span>
                   </div>
                   <div className="downloadLink">
                     <a href={song.song_file} style={this.props.linkStyle} download>download</a>
